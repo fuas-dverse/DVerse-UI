a06f51e910560e16d73c3d57630d54105f4862e6
Brett Mulder <brettmulder@Bretts-MacBook-Pro.local>
2024-05-29 10:55:21 +0200

    Chanegd auth code because we use the cloud based version now

--- auth.ts
+++ auth.ts
@@ -2,11 +2,19 @@ import NextAuth from "next-auth";
 import Keycloak from "@auth/core/providers/keycloak";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Keycloak],
+    providers: [
+        Keycloak
+    ],
     callbacks: {
-        redirect: async () => {
-            return "/";
-        }
+        async redirect({ url, baseUrl }) {
+            return baseUrl + "/chat";
+        },
+        async session({ session, token }) {
+            return session;
+        },
+        async jwt({ token, user, account }) {
+            return token;
+        },
     },
     secret: process.env.AUTH_SECRET,
 })
