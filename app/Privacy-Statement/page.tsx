8f5654767ca955c5e5e2c111f4a7aebffdb94822
Arenco Meevissen <479450@student.fontys.nl>
2024-06-04 14:07:08 +0200

    added privacy statement

---
+++ app/Privacy-Statement/page.tsx
@@ -0,0 +1,160 @@
+export default function PrivacyStatement(){
+
+return (
+    <div className="w-[700px] min-h-full justify-items-center justify-center ml-[20%] space-y-4 text-lg">
+        <h1 className="text-4xl font-bold">Privacy Statement</h1>
+    <p>
+        DVerse, located at -, is responsible for the processing of personal data as reflected in this privacy statement.</p>
+<ul className="list-disc">
+    <li>Contact Details:</li>
+    <li>https://dverse.com</li>
+    <li>+31 6 555 555 55</li>
+</ul>
+
+<h1 className="text-3xl font-bold">Personal data we process.</h1>
+<p>DVerse processes your personal data because you make use of our services and/or because you provide them to us yourself.</p>
+
+<p>Below you will find an overview of the personal data we process:</p>
+<ul className="list-disc">
+    <li>First and last name</li>
+    <li>Gender</li>
+    <li>E-mail</li>
+    <li>IP address</li>
+    <li>Information about your activities on our website</li>
+    <li>Internet browser and device type</li>
+</ul>
+
+<h1 className="text-3xl font-bold">Special and/or sensitive personal data that we process.</h1>
+<p>Our website and/or service does not intend to collect data about website visitors who are younger than 16 years old.
+     Unless they have permission from parents or guardians. However, we cannot verify that a visitor is over the age of 16.
+     We encourage parents to be involved in their children's online activities in order to prevent the collection of information about 
+     children without parental consent. If you are convinced that we have collected personal data about a minor without such consent, 
+     please contact us at info@dverse.com and we will delete this information.
+     </p>
+
+<h1 className="text-3xl font-bold">For what purpose and on what basis do we process personal data?</h1>
+<p>DVerse processes your personal data for the following purposes:</p>
+<ul className="list-disc">
+    <li>To be able to call or e-mail you if this is necessary to be able to perform our services</li>
+    <li>Inform you about changes to our services and products</li>
+    <li>Enable you to create an account</li>
+    <li>DVerse also processes personal data if we are legally obliged to do so, such as data that we need for our tax return or the production of information if the court deems it necessary.</li>
+</ul>
+
+
+
+<h1 className="text-3xl font-bold">Automated decision-making</h1>
+<p>DVerse takes responsibility on the basis of automated processing and makes decisions about matters that may have (significant) consequences for individuals. <br />
+    These are decisions that are made by computer programs or systems, without a human being (e.g., an employee of DVerse) being involved.
+     DVerse uses the following computer programs or systems:</p>
+<ul className="list-disc">
+    <li>Python</li>
+    <li>
+    NextJS
+        
+    </li>
+    <ul className="list-disc ml-5">
+        <li>Middleware with security http-headers.</li>
+    </ul>
+    
+    
+</ul>
+
+<h1 className="text-3xl font-bold">How long we keep personal data.</h1>
+<p>DVerse does not store your personal data any longer than is strictly necessary to achieve the purposes for which your data is collected.
+    We use the following retention periods for the following (categories of) personal data:
+</p>
+
+<p>
+Voluntary account cancellation: your account will remain with your standard data for a maximum of 2 weeks so that legal process has time to use it. 
+This is also possible if you have changed your mind and want the account back.
+ After these 2 weeks, your personal data will be changed to substitute data.
+</p>
+
+<p>
+Legal account termination: As soon as the legal termination has been obtained, 
+your account will be deleted as soon as possible and changed by substitute data. 
+You cannot get this account back because DVerse does not know your account details itself.
+</p>
+
+<p>Cancelling a child's account: this is similar to the legal account cancellation,
+     but DVerse will first provide a different password for it and within a week the personal details will be changed to substitutes so 
+     that DVerse can use the questions asked for further processing to know what is being asked for our platform.
+</p>
+
+<h1 className="text-3xl font-bold">Sharing personal data with third parties</h1>
+
+<p>DVerse does not sell your data to third parties and only provides it if this is necessary for the execution of our agreement with you or 
+    to comply with a legal obligation. 
+    We enter into a processing agreement with companies that process your data on our behalf to ensure the same level of 
+    security and confidentiality of your data. DVerse remains responsible for this processing.
+</p>
+
+<h1 className="text-3xl font-bold">Cookies, or similar techniques, that we use</h1>
+<p>DVerse only uses technical and functional cookies. And analytical cookies that do not infringe on your privacy. 
+    A cookie is a small text file that is stored on your computer, tablet, or smartphone when you first visit this website. 
+    The cookies we use are necessary for the technical operation of the website and your ease of use. 
+    They ensure that the website works properly and remember, for example, your preferred settings. 
+    It also allows us to optimize our website. You can opt-out of cookies by setting your internet browser to stop storing cookies. 
+    In addition, you can also delete all information that has been previously stored through your browser's settings.
+</p>
+
+<h1 className="text-3xl font-bold">Viewing, modifying, or deleting data </h1>
+<p>You have the right to access, correct or delete your personal data. 
+    In addition, you have the right to withdraw your consent to the data processing or to object to the processing of your personal data by DVerse and 
+    you have the right to data portability. 
+    This means that you can submit a request to us to send the personal data we have about you in a computer file to you or another organization named by you.
+</p>
+
+<p>You can send a request for inspection, correction, deletion, data transfer of your personal data or request for withdrawal of your consent or objection to the processing of your personal data to info@dverse.com.
+</p>
+
+<p>To ensure that the request for access has been made by you, we ask you to send a copy of your proof of identity with the request.
+    In this copy, created account name, last three questions you created, your host of the server. 
+    This is to protect your privacy. We will respond to your request as soon as possible, but within four weeks.
+</p>
+
+<p>DVerse would also like to point out that you have the possibility to file a complaint with the national supervisory authority, the Dutch Data Protection Authority. 
+    You can do so via the following link: https://autoriteitpersoonsgegevens.nl/nl/contact-met-de-autoriteit-persoonsgegevens/tip-ons.
+</p>
+
+
+<h1 className="text-3xl font-bold">How we protect personal data</h1>
+<p>DVerse takes the protection of your data seriously and responds appropriately to prevent 
+    misuse, loss, unauthorized access, unwanted disclosure, and unauthorized modification. 
+    If you have the impression that your data is not properly secured or there are indications of misuse, 
+    please contact our customer service or via info@dverse.com
+</p>
+
+<h1 className="text-3xl font-bold">Information about General Data Protection Regulation (GDPR)</h1>
+<p>we may be collecting and using information from you if you are from the European Economic Area (EEA), 
+    and in this section of our Privacy Policy we are going to explain exactly how and why is this data collected,
+     and how we maintain this data under protection from being replicated or used in the wrong way.
+</p>
+
+<h1 className="text-3xl font-bold">What is GDPR?</h1>
+<p>GDPR is an EU-wide privacy and data protection law that regulates how EU residents' data is protected by companies and enhances the control
+     the EU residents have, over their personal data.
+</p>
+
+<h1 className="text-3xl font-bold">What is personal data?</h1>
+<p>Any data that relates to an identifiable or identified individual. GDPR covers a broad spectrum of information that could be used on its own, or in combination with other pieces of information, to identify a person. Personal data extends beyond a person's name or email address. Some examples include financial information, political opinions, genetic data, biometric data, IP addresses, physical address, sexual orientation, and ethnicity. The Data Protection Principles include requirements such as:</p>
+<ul className="list-disc">
+    <li>Personal data collected must be processed in a fair, legal, and transparent way and should only be used in a way that a person would reasonably expect.
+</li>
+    <li>Personal data should only be collected to fulfil a specific purpose and it should only be used for that purpose. Organizations must specify why they need the personal data when they collect it.
+</li>
+    <li>Personal data should be held no longer than necessary to fulfil its purpose.
+</li>
+    <li>People covered by the GDPR have the right to access their own personal data.
+They can also request a copy of their data, and that their data be updated, deleted, restricted, or moved to another organization.
+</li>
+</ul>
+
+<h1 className="text-3xl font-bold">Why is GDPR important?</h1> 
+<p>GDPR adds some new requirements regarding how companies should protect individuals' personal data that they collect and process. It also raises the stakes for compliance by increasing enforcement and imposing greater fines for breach. Beyond these facts it's simply the right thing to do. At DVerse we strongly believe that your data privacy is very important and we already have solid security and privacy practices in place that go beyond the requirements of this new regulation.
+</p>
+<footer className="min-h-5"></footer> {/* for space after the last paragraph*/}
+    </div>
+)
+}
